7418971af0ae5c0f153fc1a0068c70b031937791
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-09-16 10:08:27 +0300

    header added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="p-3 text-left font-bold text-xl md:text-2xl md:text-center lg:text-3xl">
+      Wordle
+    </header>
+  );
+}
+
+export default Header;
